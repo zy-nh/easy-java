2d9588e43869e28cf4ab532d40d40082dcbb90f2
zy-nh <1205282944@qq.com>
2022-11-15 00:19:26 +0800

    feat: 代码生成测试模块

---
+++ easy-java-demo/src/main/java/cn/zhuyee/RunDemoApp.java
@@ -0,0 +1,17 @@
+package cn.zhuyee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <h2>启动类</h2>
+ *
+ * <br>
+ * Created by zhuye at 2022/11/13 23:08.
+ */
+@SpringBootApplication
+public class RunDemoApp {
+  public static void main(String[] args) {
+    SpringApplication.run(RunDemoApp.class, args);
+  }
+}
