5b7f8e5df9b5dafac8929827d8bd66a9de033287
zy-nh <1205282944@qq.com>
2022-11-15 00:18:15 +0800

    feat: 代码生成核心模块

---
+++ easy-java-core/src/main/java/cn/zhuyee/RunApplication.java
@@ -0,0 +1,15 @@
+package cn.zhuyee;
+
+import cn.zhuyee.builder.BuildTable;
+
+/**
+ * <h2>启动类</h2>
+ *
+ * <br>
+ * Created by zhuye at 2022/11/14 0:13.
+ */
+public class RunApplication {
+  public static void main(String[] args) {
+    BuildTable.getTables();
+  }
+}
